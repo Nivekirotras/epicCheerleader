0c6e0286875c1355f7c6729dfdbd02633dc5c160
Kevin Sartori <kevinsartori@Kevins-M1.local>
2023-12-04 21:49:05 +0100

    change image priority

--- components/post/post-hero.tsx
+++ components/post/post-hero.tsx
@@ -13,10 +13,11 @@ const PostHero = ({post}: PostHeroProps) => {
     <div>
       <PostContent isPostPage post={post} />
       <Image 
+        priority
         className="rounded-md object-cover object-center h-[300px] md:h-[500px] mt-6"
         src={`${process.env.NEXT_PUBLIC_ASSETS_URL}${post.image}`} // The key is defined in the settings/transformation_presets in Directus
         width={1280}
-        height={5000}
+        height={500}
         alt={post.title}
         />
     </div>
