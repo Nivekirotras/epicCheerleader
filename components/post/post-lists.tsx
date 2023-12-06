6494f544b6022bba1b4073b30bee0f04d02d806e
Kevin Sartori <kevinsartori@Kevins-M1.local>
2023-12-06 18:14:52 +0100

    row auto height

--- components/post/post-lists.tsx
+++ components/post/post-lists.tsx
@@ -8,11 +8,11 @@ interface PostListProps {
 
 const PostList = ({ posts, layout = "vertical" }: PostListProps) => {
   // Set grid classes based on the number of posts
-  let gridClasses = 'grid grid-cols-1 gap-10';
+  let gridClasses = 'grid grid-cols-1 gap-10 auto-rows-min';
 
   if (posts.length >= 4) {
     gridClasses += ' md:grid-cols-2 lg:grid-cols-3'; // 3 columns on large screens, 2 on medium
-  } else if (posts.length = 2) {
+  } else if (posts.length === 2) {
     gridClasses += ' md:grid-cols-2'; // if there are only 2 posts then 2 columns on medium and larger screens
   } else {
     gridClasses += ' md:grid-cols-2 lg:grid-cols-3'; // If there are 3 posts then 3 columns on medium and larger screens
